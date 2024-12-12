cec991aad043f9d36db2749ac0f97c5994958cc7
natasyababulrhaiyan <natasyalsm@gmail.com>
2024-12-12 18:39:21 +0700

    commit natasya

--- musicplayer/music_player/lib/main.dart
+++ musicplayer/music_player/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:music_player/themes/light_mode.dart';
+import 'package:music_player/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,8 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      theme: lightMode, // test
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
 
     );
   }
